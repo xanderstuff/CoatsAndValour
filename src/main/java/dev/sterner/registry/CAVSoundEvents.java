a7d0b2d869961ee0da4445b8a464dd7d6436f46e
xanderstuff <68503800+xanderstuff@users.noreply.github.com>
2023-11-22 03:27:31 -0600

    Fix incorrect namespaces for SoundEvents, hide "Missing sound for event" warnings by disabling currently unused SoundEvents

--- src/main/java/dev/sterner/registry/CAVSoundEvents.java
+++ src/main/java/dev/sterner/registry/CAVSoundEvents.java
@@ -7,24 +7,22 @@ import net.minecraft.sound.SoundEvent;
 
 public interface CAVSoundEvents {
 
-    SoundEvent BLUNDERBUSS_CLOSE = register("blunderbuss_close");
-    SoundEvent BLUNDERBUSS_DISTANT = register("blunderbuss_distant");
-    SoundEvent BULLET_CRACK = register("bullet_crack");
-    SoundEvent CANON_CLOSE = register("canon_close");
-    SoundEvent CANON_DISTANT = register("canon_distant");
+//    SoundEvent BLUNDERBUSS_CLOSE = register("blunderbuss_close");
+//    SoundEvent BLUNDERBUSS_DISTANT = register("blunderbuss_distant");
+//    SoundEvent BULLET_CRACK = register("bullet_crack");
+//    SoundEvent CANON_CLOSE = register("canon_close");
+//    SoundEvent CANON_DISTANT = register("canon_distant");
 
-    SoundEvent COCKED = register("cocked");
-    SoundEvent MUSKET_CLOSE = register("musket_close");
-    SoundEvent MUSKET_DISTANT = register("musket_distant");
-    SoundEvent PISTOL_PRIMED = register("pistol_primed");
-    SoundEvent PISTOL_CLOSE = register("pistol_close");
-    SoundEvent PISTOL_DISTANT = register("pistol_distant");
-    SoundEvent RAMROD = register("ramrod");
+//    SoundEvent COCKED = register("cocked");
+//    SoundEvent MUSKET_CLOSE = register("musket_close");
+//    SoundEvent MUSKET_DISTANT = register("musket_distant");
+//    SoundEvent PISTOL_PRIMED = register("pistol_primed");
+//    SoundEvent PISTOL_CLOSE = register("pistol_close");
+//    SoundEvent PISTOL_DISTANT = register("pistol_distant");
+//    SoundEvent RAMROD = register("ramrod");
 
     static SoundEvent register(String id) {
-        SoundEvent event = SoundEvent.of(CoatsAndValour.id(id));
-        Registry.register(Registries.SOUND_EVENT, id, event);
-        return event;
+        return Registry.register(Registries.SOUND_EVENT, CoatsAndValour.id(id), SoundEvent.of(CoatsAndValour.id(id)));
     }
 
     static void init() {
